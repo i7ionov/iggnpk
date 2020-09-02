8ac6119b9629a443fbce123341607b4a16e9704a
ivsemionov <ivsemionov@iggn.permkrai.ru>
2020-09-02 10:32:09 +0500

    обновление devextreme

--- iggnpk/ng-iggnpk/src/main.ts
+++ iggnpk/ng-iggnpk/src/main.ts
@@ -1,12 +1,14 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import themes from 'devextreme/ui/themes';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+themes.initialized(() => {
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+});
